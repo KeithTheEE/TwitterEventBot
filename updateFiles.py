3b7d9668ae32e5441fedfaa42a812b39652f055e
keith <kmurrayis@gmail.com>
2017-09-20 03:39:02 -0500

    Adding an error logger which should email me, then restart the bot

--- updateFiles.py
+++ updateFiles.py
@@ -6,6 +6,7 @@ Keith Murray
 '''
 
 import subprocess
+import traceback
 import socket
 import sys
 import os
@@ -40,4 +41,46 @@ process = subprocess.Popen(["git", "pull", "origin", "master"], stdout=subproces
 output = process.communicate()[0]
 print output
 
-import crowdSourceEvents
+emailCount = 0
+emailLimit = 5
+
+while True:
+    try:
+        import crowdSourceEvents
+    except KeyboardInterrupt:
+        break
+    except:
+        ln = traceback.format_exc()
+        flname = 'errorReport.log'
+        fl = open(flname, 'w')
+        fl.write(ln)
+        fl.close()
+        import kmmessage        
+        recip = 'kmurrayis@gmail.com'
+        subject = 'Error on the Twitter Bot'
+        text = ln
+        if emailCount < emailLimit:
+            try:
+                kmmessage.message_Send_Full_Email([recip], subject, text, files=[flname])
+            except:
+                trace = traceback.format_exc()
+                flname = 'moreErrors.log'
+                fl = open(flname, 'a')
+                fl.write(ln + '\n' + trace + '\n')
+                fl.close()
+            emailCount += 1
+        if emailCount == emailLimit:
+            text = text + '\n' + "EMAIL LIMIT REACHED FOR THIS BOOT"
+            try:
+                kmmessage.message_Send_Full_Email([recip], subject, text, files=[flname])
+            except:
+                trace = traceback.format_exc()
+                flname = 'moreErrors.log'
+                fl = open(flname, 'a')
+                fl.write(ln + '\n' + trace + '\n')
+                fl.close()
+            emailCount += 1
+            
+        
+
+        
